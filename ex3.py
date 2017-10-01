6d973d6720c49413dc921caac52e31e6013ac72a
Adel Shehadeh <adelshehadeh@Adels-MacBook-Pro.local>
2017-10-01 12:30:21 +0200

    added some comments

--- ex3.py
+++ ex3.py
@@ -1,18 +1,23 @@
+#Intro on what I am going to do
 print ("I will now count my chickens:")
 
+#These are my hens and roosters counts
 print ("Hens", 25 + 30 / 6)
 print ("Roosters", 100 - 25 * 3 % 4)
 
 print ("Now I will count the eggs:")
 
+#An interesting expression
 print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print ("Is it true that 3 + 2 < 5 -7?")
 
+#Comparison
 print (3 + 2 < 5 - 7)
 
 print ("What is 3 + 2?", 3 + 2)
 print ("What is 5 - 7?", 5 - 7)
+
 
 print ("Oh, that's why it's false.")
 
