fd03a70850c95cb5799a1922b33c7f86e42187d8
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-10-03 14:19:17 +0200

    adding ex5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = "Adel Shehadeh"
+age = 36 # not a lie
+height = 72.04 # inches
+weight = 180 # lbs
+eyes = 'brown'
+teeth = 'white'
+hair = 'brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height * 2.45} cm tall.") # convert to cm
+print(f"He's {weight * .453} kg heavy") # convert to kg
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"He's teeth are usually {teeth} depending on the coffee.")
+
+#This line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, {weight} I get {total}.")
