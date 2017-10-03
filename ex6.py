230c41e64efd0e3c326802d5e0b963163323640f
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-10-03 15:21:35 +0200

    solving ex6 study drills

--- ex6.py
+++ ex6.py
@@ -1,24 +1,37 @@
+#assigning 10 to types of people
 types_of_people = 10
+#assigning a formatteds tring to x
 x = f"There are {types_of_people} types of people."
 
 #assign "binary" to binary
 binary = "binary"
+#assign a string to do_not
 do_not = "don't"
 #assign a formatted string to y
 y = f"Those who know {binary} and those who {do_not}."
 
+#print out x
 print(x)
+
+#print out y
 print(y)
 
+#print out a formatted string
 print(f"I said: {x}")
+
+#print out a formatted string
 print(f"I also said: {y}")
 
+#set my evaluation of the joke
 hilarious = False
 
-joke_evaluation = "Isn;t that joke so funny? {}" #{} used in loops to format
+#create a formatted st
+joke_evaluation = "Isn't that joke so funny? {}" #{} used in loops to format
 print(joke_evaluation.format(hilarious))
 
+#assign a text to w and e
 w = "This is the left side of..."
 e = "a string with a right side."
 
+#print the result of concatenation of w and e
 print(w + e)
