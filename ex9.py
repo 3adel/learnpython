7f11e32531a17ac8c09a3edb79c6211d747c0624
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-10-16 17:19:31 +0200

    solve ex9

--- ex9.py
+++ ex9.py
@@ -1,12 +1,19 @@
+#Assign a string to the parameter days
 days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#Assign a string with an escape sequence to the variable months
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+#print out a string followed by another string, days
 print("Here are the days: ", days)
+
+#print out a string followed by another string, months
 print ("Here are the months: ", months)
 
+
 #using 3 double-quotes is useful for long text.
-print(""""There's something going on here.
+print("""There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
-Even 4 lines if we want to, or 5, or 6.
+Even lines if we want to, or 5, or 6.
 """)
