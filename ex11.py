0615a261d67265b4a125a3cff4337f35e8bf32f3
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-10-20 12:22:50 +0200

    solving ex11

--- ex11.py
+++ ex11.py
@@ -10,6 +10,17 @@ print(f"So, you're {age} old. {height} tall and {weight} heavy.")
 #We use repr() to show the python representation of a variable value
 #We use casting int(value) to convert it into an int
 
-#Study drill 1
+#Study drill 2 and 3
+print("What's your age?", end=' ')
+age = input("--->")
 
+#3 ways to do the same thing:
+print("Your name is " + age)
+print(f"Your age is {age}")
+print("Your age is {}".format(age))
+
+
+
+
+       
 
