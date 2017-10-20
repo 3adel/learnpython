aab827ff275cba89484c982f4d562f2225e127a7
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-10-20 12:10:30 +0200

    started ex11

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print("How old are you?", end=' ')
+age = input("---> ")
+print("How tall are you?", end =' ')
+height = input("---> ")
+print("How much do you weigh?", end=' ')
+weight = input("---> ")
+
+print(f"So, you're {age} old. {height} tall and {weight} heavy.")
+
+#We use repr() to show the python representation of a variable value
+#We use casting int(value) to convert it into an int
+
+#Study drill 1
+
+
