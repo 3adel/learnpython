d66f0136d4a5d1d9dfc9cbd04712e0713752ccb1
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-10-21 19:43:47 +0200

    all argument expressions are evaluated before the call is attempted check https://docs.python.org/3/reference/expressions.html#calls

--- ex12.py
+++ ex12.py
@@ -3,3 +3,6 @@ height = input(f"You are {age} years old. Now tell us how tall are you?")
 weight = input("How much do you weigh?")
 
 print(f"So, you're {age} old. {height} tall and {weight} heavy.")
+
+#all argument expressions are evaluated before the call is attempted. This is why input() function is called first in the print statement
+print("How old are you?" , input()) #initially, input() has no value
