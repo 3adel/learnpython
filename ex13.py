749721dc06eb740c9221730d36fc65fb985ad7ab
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-10-22 11:34:58 +0200

    solving ex13-2.b

--- ex13.py
+++ ex13.py
@@ -2,12 +2,14 @@ from sys import argv
 #read the WYSS section for how to run this
 
 #unpack argv. Take whatever in argv and assign it to those 4 variables
-script, firstName, lastName, age = argv
+script, city, population, country, continent, planet = argv
 
 #What's in argv?
 print(">>>> argv holds this: ", repr(argv))
 
 print("The script is called:", script)
-print("Your first variable is:", firstName)
-print("Your second variable is:", lastName)
-print("Your second variable is:", age)
+print("Your city is:", city)
+print("Your city population is:", population)
+print("Your country is:", country)
+print("Your continent is:", continent)
+print("Your planet is:", planet)
