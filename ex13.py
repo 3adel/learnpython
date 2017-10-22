21585fe632ad4708b64008bde091af4acee29f8e
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-10-22 11:38:29 +0200

    solving ex13-3

--- ex13.py
+++ ex13.py
@@ -2,7 +2,7 @@ from sys import argv
 #read the WYSS section for how to run this
 
 #unpack argv. Take whatever in argv and assign it to those 4 variables
-script, city, population, country, continent, planet = argv
+script, city, population, country, continent = argv
 
 #What's in argv?
 print(">>>> argv holds this: ", repr(argv))
@@ -12,4 +12,7 @@ print("Your city is:", city)
 print("Your city population is:", population)
 print("Your country is:", country)
 print("Your continent is:", continent)
-print("Your planet is:", planet)
+
+#get the planet using input() instead of argv
+planet = input("What's your planet?")
+print("Your planet is: ", planet)
