e1c9f60842ff385e5fa2ffb4ae8c47a75b7a7905
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-10-25 11:15:18 +0200

    solving ex15 until 5

--- ex15.py
+++ ex15.py
@@ -1,15 +1,22 @@
+#import the argv module so we can pass arguments through commandline
 from sys import argv
 
-script, filename = argv
+script, filename = argv #define commandline arguments
 
-#txt = open(filename)
 
-print(f"Here's your file: {filename}")
-print(open(filename).read())
+#txt = open(filename) #open the file
 
-print("Type te filename again:")
-file_again = input("> ")
+#Show what's in the file object
+print(">>>>>> File object: ",repr(open(filename)))
 
-txt_again = open(file_again)
 
-print(txt_again.read())
+print(f"Here's your file: {filename}") #show the file name
+print(open(filename).read()) #open the file, then read it and print it.
+
+print("Type the filename again:") #acquire filename through the prompt
+file_again = input("> ") #prompt the user to enter a file name
+
+txt_again = open(file_again) #open the file again
+
+print(txt_again.read()) #read the file, then print it.
+
