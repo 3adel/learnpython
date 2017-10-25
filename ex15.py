ec1cd3d8f9650601f9537094b6dea9f588fa603a
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-10-25 11:22:05 +0200

    added some comments

--- ex15.py
+++ ex15.py
@@ -1,10 +1,11 @@
-#import the argv module so we can pass arguments through commandline
+#import the argv module from the sys package so we can pass arguments through commandline
 from sys import argv
 
 script, filename = argv #define commandline arguments
 
 
 #txt = open(filename) #open the file
+#txt_again.close() #close the files when you're done with them
 
 #Show what's in the file object
 print(">>>>>> File object: ",repr(open(filename)))
@@ -17,7 +18,10 @@ print("Type the filename again:") #acquire filename through the prompt
 file_again = input("> ") #prompt the user to enter a file name
 
 txt_again = open(file_again) #open the file again
-
 print(txt_again.read()) #read the file, then print it.
+txt_again.close()
+
+#txt_again.close() #close the files when you're done with them
+
 
 #You can open files from the python shell: >>> open("ex3.py").read()
