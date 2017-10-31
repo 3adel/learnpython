a65a867e699a9d23ba69065a86257a9a0e8bdebe
Adel Shehadeh <adelshehadeh@Adels-MacBook-Pro.local>
2017-10-31 11:01:07 +0100

    some cleanup of ex19.py

--- ex19.py
+++ ex19.py
@@ -6,7 +6,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
     print("Get a blanket.")
-    #indicate that the function exitted
+    #indicate that the function exited
     print("<<<< Exit cheese_and_crackers function.\n")
 
 #first call of the function with passing just numbers
@@ -22,9 +22,9 @@ amount_of_crackers = 50
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 #call the function with some math as arguments
-print("WE can even do math inside too:")
+print("We can even do math inside too:")
 cheese_and_crackers(10 + 20, 5 + 6)
 
-#call the function with some math that includes global variables 
+#call the function with some math that includes global variables
 print("And we can combine the two, variables and math:")
 cheese_and_crackers(amount_of_cheese + 10, amount_of_crackers + 1000)
