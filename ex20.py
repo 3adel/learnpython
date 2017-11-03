ca7cf7e573f3a76424c1b0496a2672a99ca59890
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-11-03 17:41:27 +0100

    solving ex20-1

--- ex20.py
+++ ex20.py
@@ -1,14 +1,17 @@
+#import the argv module from the sys package
 from sys import argv
 
+#assign argv to script name and the name of the input file. This way you can pass them as arguments when you run the script
 script, input_file = argv
 
+#this function takes a file object and prints its content to the console
 def print_all(f):
     print(f.read())
     
-
+#this fucntion takes a file object and moves the cursor to its beginning
 def rewind(f):
     f.seek(0)
-    
+#this function takes the line number and the file object as arguments, then prints the given line number and the content of the file at that specific line   
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
