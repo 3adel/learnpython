76faa3c046e63ea01009af4d53a47451e4911671
Adel Shehadeh <adelshehadeh@Adels-MacBook-Pro.local>
2017-11-04 22:20:33 +0100

    solved sommon student question: Why are there empty lines between the lines in the file?

--- ex20.py
+++ ex20.py
@@ -7,14 +7,14 @@ script, input_file = argv
 #this function takes a file object and prints its content to the console
 def print_all(f):
     print(f.read())
-    
+
 #this fucntion takes a file object and moves the cursor to its beginning
 def rewind(f):
     f.seek(0)
-#this function takes the line number and the file object as arguments, then prints the given line number and the content of the file at that specific line   
+#this function takes the line number and the file object as arguments, then prints the given line number and the content of the file at that specific line
 def print_a_line(line_count, f):
     print(">>>> Passed line count is:", line_count)
-    print(line_count, f.readline())
+    print(line_count, f.readline(), end = "")
 
 #open the file and store the file object in the variable current_file
 current_file = open(input_file)
@@ -42,4 +42,3 @@ print_a_line(current_line, current_file)
 #update the value of current file
 current_line += 1
 print_a_line(current_line, current_file)
-
