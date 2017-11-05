b0cd0c63bc4950ee5271367239d54c8f37f8ae11
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-11-05 11:52:00 +0100

    ex21, this way you can utilize input() in a function call

--- ex21.py
+++ ex21.py
@@ -28,6 +28,7 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 #A puzzle for extra credit, type it in anyway
 print("Here is a puzzle.")
 
-what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+what = add(float(input("Age:")), subtract(float(input("Height:")), multiply(float(input("Weight:")), divide(int(input("IQ:")), 2))))
 
 print("That becomes: ", what, "Can you do it by hand?")
+#what = add(age, -4426))
