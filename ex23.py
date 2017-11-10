5e5ddfb72f2d4c3eb15e8bc791fb5b9bf0d4eec4
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2017-11-10 18:14:18 +0100

    added more tracing

--- ex23.py
+++ ex23.py
@@ -1,29 +1,30 @@
 import sys
 script, input_encoding, error = sys.argv
 
-
 #Main function
-def main(language_file, encoding, errors):
+def main(language_file, encoding, errors, counter):
     line = language_file.readline()
 
     if line:
-        print(">>>> Line is: ", line, end = "")
+        print(f"Line #{counter}")
+        print(">>>> Line:", line, end = "")
         print_line(line, encoding, errors)
-        return main(language_file, encoding, errors)
+        counter +=1
+        return main(language_file, encoding, errors, counter)
 
 #This will encode strings, decode them again and then print them
 def print_line(line, encoding, errors):
-    print(">>>> Line in print_line is: ", line, end = "")
+    print(">>>> Line in print_line:", line, end = "")
     next_lang = line.strip() #remove leading and trailing spaces
-    print(">>>> Stripped line in print_line is: ", next_lang)
+    print(">>>> Stripped line in print_line:", next_lang)
 
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
-    print(">>>> Raw byte: ",raw_bytes)
-    print(">>>> Cooked string: ",cooked_string, "\n")
+    print(">>>> Raw byte:",raw_bytes)
+    print(">>>> Cooked string:",cooked_string, "\n")
 
 #start
 languages = open("languages.txt", encoding="utf-8")
 
-main(languages, input_encoding, error)
+main(languages, input_encoding, error, 1)
