2c3fb12cf037cb5df54dd9d9a755f4bf513d8d0c
Adel Shehadeh <adel.sh@gmail.com>
2017-11-30 17:49:07 +0100

    Update ex25.py

--- ex25.py
+++ ex25.py
@@ -18,14 +18,21 @@ def print_last_word(words):
     print(word)
 
 def sort_sentence(sentence):
-    print(">>>> sentence passed: ", sentence)
+    #print(">>>> sentence passed: ", sentence)
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    print(">>>> sentence broken: ", words)
-    print(">>>> sentence sorted: ", sort_words(words))
+    #print(">>>> sentence broken: ", words)
+    #print(">>>> sentence sorted: ", sort_words(words))
     return sort_words(words)
 
 def print_first_and_last(sentence):
-    
+    """Sorts the words then prints the first and last one."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
 
-print(sort_sentence("This is a long sentence"))
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
