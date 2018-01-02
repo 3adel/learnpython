fd59ba27883ef7bcbcc43d250286c722c6db2f87
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-01-02 17:09:10 +0100

    finished ex2

--- ex31-study-drill2.py
+++ ex31-study-drill2.py
@@ -4,7 +4,8 @@ print("Enter the case:")
 case = input("->")
 
 def casePronoun(pronoun, case):
-    if pronoun == "I" or "i":
+    if pronoun == "I" or pronoun == "i":
+        print(">>> first if met")
         if case == "nom":
             return "ich"
         elif case == "akk":
@@ -12,8 +13,8 @@ def casePronoun(pronoun, case):
         elif case == "dat":
             return "mir"
         else:
-            return "Invalid case Entered!"
-    elif pronoun == "you":
+            return "Invalid case entered"
+    elif pronoun == "you" or pronoun == "You":
         if case == "nom":
             return "du"
         elif case == "akk":
@@ -21,11 +22,82 @@ def casePronoun(pronoun, case):
         elif case == "dat":
             return "dir"
         else:
-            return "Invalid case Entered!"
-    else:
-        print("no")
+            return "Invalid case entered"
 
-    return "Unknown"
+    elif pronoun == "he" or pronoun == "He":
+        if case == "nom":
+            return "er"
+        elif case == "akk":
+            return "ihn"
+        elif case == "dat":
+            return "ihm"
+        else:
+            return "Invalid case entered"
+
+    elif pronoun == "she" or pronoun == "She":
+        if case == "nom":
+            return "sie"
+        elif case == "akk":
+            return "sie"
+        elif case == "dat":
+            return "ihr"
+        else:
+            return "Invalid case entered"
+
+    elif pronoun == "it" or pronoun == "It":
+        if case == "nom":
+            return "es"
+        elif case == "akk":
+            return "es"
+        elif case == "dat":
+            return "ihm"
+        else:
+            return "Invalid case entered"
+
+    elif pronoun == "we" or pronoun == "We":
+        if case == "nom":
+            return "wir"
+        elif case == "akk":
+            return "uns"
+        elif case == "dat":
+            return "uns"
+        else:
+            return "Invalid case entered"
+
+    elif pronoun == "y'all" or pronoun == "Y'all":
+        if case == "nom":
+            return "ihr"
+        elif case == "akk":
+            return "euch"
+        elif case == "dat":
+            return "euch"
+        else:
+            return "Invalid case entered"
+
+    elif pronoun == "they" or pronoun == "They":
+        if case == "nom":
+            return "sie"
+        elif case == "akk":
+            return "sie"
+        elif case == "dat":
+            return "ihnen"
+        else:
+            return "Invalid case entered"
+
+    elif pronoun == "you f" or pronoun == "You f":
+        if case == "nom":
+            return "Sie"
+        elif case == "akk":
+            return "Sie"
+        elif case == "dat":
+            return "Ihnen"
+        else:
+            return "Invalid case entered"
+
+    else:
+        return "Invalid pronoun"
+
+    return "Unknown error"
 
 
 print("The conjugation of [{}] in the [{}] case is [{}]".format(pronoun, case, casePronoun(pronoun, case)))
