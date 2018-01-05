361f07d9b9a1594782c568f1f46023a6cbaaa243
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-01-05 17:40:02 +0100

    showing that simultaneous addition can help with fibonacci series

--- ex32-Fibonacci.py
+++ ex32-Fibonacci.py
@@ -1,4 +1,13 @@
 a, b = 0, 1
-while b < 1000000:
-    print(b, end='\n')
+while b < 10:
+    print(">>> a before = ",a)
+    print(">>> b before = ",b)
+    #with simultaneous addition
     a, b = b, a+b
+    
+    #without simultaneous addition
+    #a = b
+    #b = a + b
+    
+    print(">>> a after = ",a)
+    print(">>> b after = ",b,end='\n\n')
