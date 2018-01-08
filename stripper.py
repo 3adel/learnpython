6af6386a5c40ac8adc43a606a415d923d3202ad5
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2018-01-08 22:12:40 +0100

    add stripper script. This will strip a text from its trailing new lines

---
+++ stripper.py
@@ -0,0 +1,11 @@
+i = """Die Umgangsformen in der Schweiz, Österreich und
+Deutschland sind im Großen und Ganzen ähnlich. Zur
+Begrüßung ist es üblich, die Hand zu geben. Wenn man
+jemanden gut kennt, ist auch eine Umarmung möglich.
+In der jüngeren Generation ist es modern geworden,
+Begrüßungsküsse zu geben. Das ist aber nur im
+Privatleben möglich, im Geschäftsleben ist körperliche
+Nähe tabu."""
+
+j = i.rstrip()
+print(j)
