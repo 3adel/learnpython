35e01acbaca3b9c6c7f05c3b1d8084c727e52cd4
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-01-25 17:54:40 +0100

    more games

--- space_adventure.py
+++ space_adventure.py
@@ -13,4 +13,15 @@ def displayInto():
     print("This is an intro to the game.")
     print()
 
+def choosePath():
+    path = ""
+    #input validation trick
+    while path !=1 and path != 2:
+        try:
+            path = int(input("Which path will you shooose? (1 or 2) "))
+        except:
+            print("Learn to type a number!")
+    return path
+
 displayInto()
+choosePath()
