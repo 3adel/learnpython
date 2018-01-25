22ab0167b41260f48a731a45a4bcfbc14fa68eef
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-01-25 17:40:09 +0100

    ex 36 text adventure game
    Pleas enter the commit message for your changes. Lines starting

---
+++ space_adventure.py
@@ -0,0 +1,16 @@
+# Adel Shehadeh
+# 2018-01-25
+# Text-based space adventure game
+
+import random
+import time
+
+print("Hello")
+time.sleep(3)
+print("There")
+
+def displayInto():
+    print("This is an intro to the game.")
+    print()
+
+displayInto()
