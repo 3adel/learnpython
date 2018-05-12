f60245dbc1fc534c45348fc6c9fb430e420dc476
Adel Shehadeh <adel.sh@gmail.com>
2018-05-12 11:06:15 +0200

    example of using list comprehensions to combine two lists

--- ex38-lists-comprehension.py
+++ ex38-lists-comprehension.py
@@ -1,6 +1,18 @@
-squares = list(map(lambda x: x**2, range(10)))
-print(squares)
+#example of using list comprehensions to combine two lists
 
-#or equivvalently:
-squares2 = [x**2 for x in range(10)]
-print(squares2)
+#traditional method:
+combs = []
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x != y:
+            combs.append((x,y))
+
+print(">>> x",x)
+print(">>> y", y)
+print("Comb1",combs)
+
+#with list comprehensions
+combs2 = [(m,z) for m in [1,2,3] for z in [3,1,4] if m != z]
+print(">>> m",x)
+print(">>> z", y)
+print("Comb2",combs2)
