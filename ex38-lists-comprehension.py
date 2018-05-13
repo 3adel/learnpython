78b551ff1bfea67769c424d01ef0d6614bec0831
Adel Shehadeh <adel.sh@gmail.com>
2018-05-13 08:22:14 +0200

    #how to flatten a list:

--- ex38-lists-comprehension.py
+++ ex38-lists-comprehension.py
@@ -24,3 +24,19 @@ print(">>> freshFruitCleaned", freshFruitCleaned)
 #create a list of 2-tuple like (number, square)
 squaresTuple = [(x,x**2) for x in range(500)]
 print("squaresTuple", squaresTuple)
+
+#how to flatten a list:
+    #Traditional method:
+
+myTwoDList = [[1,2,3],[4,5,6],[7,8,9]]
+myFlatList = []
+for elem in myTwoDList:
+    for num in elem:
+        myFlatList.append(num)
+
+print(myFlatList)
+
+    #with list comprehension
+
+myFlatList2 = [num for elem in myTwoDList for num in elem]
+print(myFlatList2)
