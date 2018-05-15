243b154414ec54e2f3bf3a30e414f320110770f6
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-05-15 10:27:11 +0200

    transpose with python

--- ex38-lists-comprehension.py
+++ ex38-lists-comprehension.py
@@ -1,5 +1,14 @@
-#list values on pi with list comprehensions
-from math import pi
-i = int(input("Enter i --->"))
-piList = [round(pi, i) for k in range(i)]
-print(piList)
+#   Transpose a matrix with nested list comprehensions
+
+
+
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+transposed = [1,2,3,4]
+print(">>>matrix: ", matrix)
+
+for i in range(len(matrix[0])):
+    for j in range (len(matrix)):
+            #print("i",matrix[j][i],)
+            transposed[i][j]=matrix[j][i]
+
+print(transposed)
