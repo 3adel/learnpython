740c79e9740856c820542c0fd4215f622febb312
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-05-23 17:55:29 +0200

    what is a module

---
+++ ex40.py
@@ -0,0 +1,5 @@
+# modules are nothing but python files that contain functions or variables, in this case, it's mystuff.py in the same folder
+import mystuff
+mystuff.apple()
+
+print(mystuff.tangerine)
