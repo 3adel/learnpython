4dea4dc7b8f096910f381acb77cd3a9a8c34cb4c
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-05-23 18:01:22 +0200

    how to define a class and intantiate it

--- ex40.py
+++ ex40.py
@@ -1,5 +1,13 @@
-# modules are nothing but python files that contain functions or variables, in this case, it's mystuff.py in the same folder
-import mystuff
-mystuff.apple()
+# to create a class that resembles the module behaviour:
 
-print(mystuff.tangerine)
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
