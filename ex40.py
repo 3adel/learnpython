214bc4d459d7e55985055b238a31de3f62f775f3
Adel Shehadeh <adel.shehadeh@zalando-lounge.de>
2018-05-28 17:30:38 +0200

    study drills 2

--- ex40.py
+++ ex40.py
@@ -6,9 +6,10 @@ class Song(object):
         for line in self.lyrics:
             print(line)
 
-happy_bday = Song(["Happy birthday to you",
+hSong = ["Happy birthday to you",
                    "I don't want to get sued",
-                   "So I'll stop right there"])
+                   "So I'll stop right there"]
+happy_bday = Song(hSong)
 
 bulls_on_parade = Song(["They rally round the family","With pocket full of shells"])
 
