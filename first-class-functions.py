35916a32065115ba0ac6d329c866390f50cc231c
Adel Shehadeh <adel.sh@gmail.com>
2018-06-04 08:03:31 +0200

    returning a function

--- first-class-functions.py
+++ first-class-functions.py
@@ -2,22 +2,15 @@
 # A function is described as first class when it's treated in the realm of the prgramming languag as first class citizen. Meaning it can be returned, passed as an argument or assigned to a variable
 
 
-def square(x):
-	return x ** 2
+def logger(msg):
 
-def cube(x):
-	return x ** 3
+	def log_message():
+		print('Log:', msg)
 
-# A sample map function. It takes a function as an argument
-def my_map(func, arg_list):
-	result = []
-	for i in arg_list:
-		result.append(func(i))
-	return result
+	return log_message
 
-#note that when we're passing square function as an argument, we don't include the parenthesis. The reason is that we are not execution the function square, but rather we're just passing it to another function.
-squares = my_map(square, [1, 2, 3, 4, 5])
-print(squares)
+log_hi = logger('Hi')
+print(">>> log _hi is actually a function now: ",log_hi)
 
-cubes = my_map(cube, [1, 2, 3, 4, 5])
-print(cubes)
+#now we can execute log_hi. Notice how log_hi remembered our messag passed to it, i.e., "Hi"
+log_hi()
