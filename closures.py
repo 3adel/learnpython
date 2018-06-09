779a8faea5bd8c1810fb3df725943b9399723c11
Adel Shehadeh <adel.sh@gmail.com>
2018-06-09 13:41:25 +0200

    closure taking a parameter

--- closures.py
+++ closures.py
@@ -1,6 +1,6 @@
-def outer_func():
+def outer_func(msg):
 	#the variable message is so called free variable
-	message = 'Hi'
+	message = msg
 
 	#a closure in simple terms is an inner function that remembers and has access to the
 	#variables in local scope in which it was created
@@ -13,8 +13,8 @@ def outer_func():
 	#here, we return the function executed
 	#return inner_func()
 
-my_func = outer_func()
-my_func()
+hi_func =  outer_func('Hi')
+hello_func = outer_func('Hello')
 
-print(my_func.__name__)
-print(outer_func().__name__)
+hi_func()
+hello_func()
