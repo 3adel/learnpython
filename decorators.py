cdf206e46bc073a5803d1805c8d4e4ae85d25925
Adel Shehadeh <adel.sh@gmail.com>
2018-06-12 06:20:13 +0200

    a simple decorator function

--- decorators.py
+++ decorators.py
@@ -1,10 +1,17 @@
+#decorator functions allows us to easily add functionality to our existing function by adding it inside our wrapper
+
 def decorator_function(original_function):
 	def wrapper_function():
+		print('---> extra functionality in wrapper function ran')
 		return original_function()
 	return wrapper_function
 
+@decorator_function
 def display():
-	print('>>> display function ran')
+	print('---> display function ran')
 
-decorated_display = decorator_function(display)
-decorated_display()
+#or can be written without the @ directive instead as: 
+#display = decorator_function(display)
+
+display()
+
