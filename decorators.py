98bb3af59595516345948971a7d9c37e1bb8ab40
Adel Shehadeh <adel.sh@gmail.com>
2018-06-13 06:21:22 +0200

    decorators can be used as loggers to multiple functions

--- decorators.py
+++ decorators.py
@@ -1,3 +1,5 @@
+
+#this decorator can be used for any new function as a logger
 def my_logger(orig_func):
 	import logging
 	logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
@@ -13,4 +15,10 @@ def my_logger(orig_func):
 def display_info(name, age):
 	print('---> display_info ran with arguments ({}, {})'.format(name, age))
 
+@my_logger
+def add_numbers(x, y):
+	print('---> add_numbes ran with arguments ({}, {})'.format(x, y))
+	return x + y
+
 display_info('John', 25)
+add_numbers(1, 2)
