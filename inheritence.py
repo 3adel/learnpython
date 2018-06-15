c0c645f208475b8583a07f744acb2683cdf4f8ed
Adel Shehadeh <adel.sh@gmail.com>
2018-06-15 08:19:04 +0200

    another example of inheritence

--- inheritence.py
+++ inheritence.py
@@ -15,13 +15,49 @@ class Employee:
 	def apply_raise(self):
 		self.pay = int(self.pay * self.raise_amount)
 
-#inheritence
+#subclassing will change the raise ammount and add a programming language and 
 class Developer(Employee):
 	raise_amount = 1.1
 
-dev_1 = Developer('Corey', 'Schafer', 50000)
-dev_2 = Developer('Adel', 'Shehadeh', 120000)
+	def __init__(self, first, last, pay, prog_lang):
+		super().__init__(first, last, pay)
+		# or:
+		#Employee.__init__(self, first, last, pay)
+		self.prog_lang = prog_lang
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+#a manager will manage a list of employees
+class Manager(Employee):
+	def __init__(self, first, last, pay, employees=None):
+		super().__init__(first, last, pay)
+		if employees is None:
+			self.employees = []
+		else: 
+			self.employees = employees
+
+	def add_emp(self, emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
+
+	def remove_emp(self, emp):
+		if emp in self.employees:
+			self.employees.remove(emp)
+
+	def print_emp(self):
+		for emp in self.employees:
+			print('-->', emp.fullName())
+
+		
+
+
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'python')
+dev_2 = Developer('Adel', 'Shehadeh', 120000, 'java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print('--> Manager Email',mgr_1.email)
+print(mgr_1.print_emp())
+mgr_1.add_emp(dev_2)
+print(mgr_1.print_emp())
+
+
