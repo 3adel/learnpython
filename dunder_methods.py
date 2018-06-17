aaa0f79f1fa974ff76306c2b28b886cf2f1df980
Adel Shehadeh <adel.sh@gmail.com>
2018-06-17 11:21:44 +0200

    more on dunder methods

--- dunder_methods.py
+++ dunder_methods.py
@@ -14,12 +14,29 @@ class Employee:
 	def apply_raise(self):
 		self.pay = int(self.pay * raise_amt)
 
+	#this is the str() fallbackmethod when you try to print the object
+	def __repr__(self):
+		return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+
+	def __str__(self):
+		return '{} - {}'.format(self.fullname(), self.email)
+
+
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Adel', 'Shehadeh', 20000)
 
-print(repr(emp_1))
+#it calls __str__() first, then if doesn't exist, falls back to __repr__()
+print(emp_1)
+
 print(str(emp_1))
+print(repr(emp_1))
+
+#same as
+print(emp_1.__str__())
+print(emp_1.__repr__())
 
 
+ 
 
