dc2cf7dc47586366ec6aa59c9e95e42f3bfb2fa3
Adel Shehadeh <adel.sh@gmail.com>
2018-06-23 13:55:17 +0200

    and example of aggregation

--- class-with-inheritance.py
+++ class-with-inheritance.py
@@ -1,4 +1,4 @@
-#composition example
+#aggregation example
 class Salary: 
 	def __init__(self, pay):
 		self.pay = pay
@@ -11,13 +11,14 @@ class Employee:
 	def __init__(self, pay, bonus):
 		self.pay = pay
 		self.bonus = bonus
-		#we are saying here that an Employee has a salary.
-		self.obj_salary = Salary(self.pay)
 
 	def annual_salary(self):
 		return self.obj_salary.get_total() + self.bonus
 
-obj_emp = Employee(100, 10)
-print(obj_emp.annual_salary())
+obj_sal = Salary(100)
+print(obj_sal.__dict__)
+
+obj_emp = Employee(obj_sal, 10)
+print(obj_emp.__dict__)
 
 #This is a simple example of composition. A Salary cannot exist on its own. You need an employee to be able to have a salary. If we destroy the employee, the salary will be also destroyed. So a asalary is tightly coupled to an employee
