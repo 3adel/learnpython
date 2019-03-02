5c7a5bf1ce0669f0b8674bfb22646fa85001bb3b
Adel Shehadeh <adel.sh@gmail.com>
2019-03-02 19:22:35 +0100

    how to solve a linear system with numpy

--- numpy-example.py
+++ numpy-example.py
@@ -1,7 +1,31 @@
 import numpy as np
 
-A = [[1, 2, 3],
-	 [4, 0, 6],
-	 [7, 8, 9]]
+#solve the equation A x r = s
+# r = [a,
+# 	 b,
+# 	 c]
+
+A = [[4, 6, 2],
+	 [3, 4, 1],
+	 [2, 8, 13]]
+
+#This represents t
+s = [9,
+	 7,
+	 2]
+
 Ainv = np.linalg.inv(A)
-print(Ainv)
+print(f"The inverse of\n{A}\nis\n{Ainv}")
+
+Adet = np.linalg.det(A)
+print(f"The determinant of\n{A}\nis\n{Adet}")
+
+
+#now solve the equation
+
+r = np.linalg.solve(A, s)
+print(f"The solution of the equation is\n{r}")
+
+
+
+
